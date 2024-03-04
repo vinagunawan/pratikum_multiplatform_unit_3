3d79755d8fcfcfac7601350c803f2fa937b6d608
vinagunawan <vinaaantaristagunawan@gmail.com>
2024-03-04 14:29:49 +0800

    commit d

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Layout Pratikum',
-      theme: ThemeData( primarySwatch: const Color.fromARGB(255, 164, 211, 249)
-      ),
+      theme: ThemeData( primarySwatch: Colors.blue),
     home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text('Pratikum Layout Flutter'),
-      )
-    body: ,
+        title:const Text('Pratikum Layout Flutter'),
+      ),
+    body: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          color: const Color.fromARGB(255, 239, 134, 126), 
+          height: 100,
+          width: 100,
+        ),
+        Container(
+          color: const Color.fromARGB(255, 146, 224, 148), 
+          height: 50,
+          width: 100,
+        ),
+        Container(
+          color: const Color.fromARGB(255, 251, 240, 144), 
+          height: 80,
+          width: 100,
+        ),
+      ],
+    ),
     );
   }
 }
+
