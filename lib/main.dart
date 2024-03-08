2315d16e9ca54fadba3a7bbdcce15dfceb28bf2d
vinagunawan <vinaaantaristagunawan@gmail.com>
2024-03-08 16:26:22 +0800

    commit g

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,80 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProfilePage());
 }
 
-class MyApp extends StatelessWidget {
+class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Layout Pratikum',
-      theme: ThemeData( primarySwatch: Colors.blue),
-    home: HomePage(),
+      title: 'Profile Page',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Profile'),
+        ),
+        body: ProfileContent(),
+      ),
     );
   }
 }
 
-class HomePage extends StatelessWidget{
-  const HomePage({super.key});
+class ProfileContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:const Text('Pratikum Layout Flutter'),
-      ),
-    body: ListView( 
-        children: <Widget>[
-        Container(
-          color: const Color.fromARGB(255, 239, 134, 126), 
-          height: 100,
-          width: 100,
+    return ListView(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Image.network(
+            'https://img.freepik.com/premium-vector/cartoon-koala-kawaii-square-animal-face-icon_8071-37416.jpg?w=740',
+            fit: BoxFit.cover,
+            width: MediaQuery.of(context).size.width,
+            height: 200,
+            ),
         ),
-        Container(
-          color: const Color.fromARGB(255, 146, 224, 148), 
-          height: 50,
-          width: 100,
+        ListTile(
+          leading: Icon(Icons.account_circle),
+          title: Text('Nama'),
+          subtitle: Text('Vina Gunawan'),
         ),
-        Container(
-          color: const Color.fromARGB(255, 251, 240, 144), 
-          height: 80,
-          width: 100,
+        ListTile(
+          leading: Icon(Icons.email),
+          title: Text('Email'),
+          subtitle: Text('vinaantaristagunawan@gmail.com'),
+        ),
+        ListTile(
+          leading: Icon(Icons.phone),
+          title: Text('No-Telepon'),
+          subtitle: Text('081-236-376-124'),
+        ),
+        ListTile(
+          leading: Icon(Icons.location_on),
+          title: Text('Alamat'),
+          subtitle: Text('Jl. Gunung Tambora no.14'),
+        ),
+        ListTile(
+          leading: Icon(Icons.book),
+          title: Text('Pogram Studi'),
+          subtitle: Text('Teknologi Informasi'),
+        ),
+        ListTile(
+          leading: Icon(Icons.content_paste),
+          title: Text('Semester'),
+          subtitle: Text('6 (Enam)'),
+        ),
+        ListTile(
+          leading: Icon(Icons.favorite),
+          title: Text('Love'),
+          subtitle: Text('Kuliner'),
+        ),
+        ListTile(
+          leading: Icon(Icons.event_note),
+          title: Text('Deskripsi'),
+          subtitle: Text('Vina Gunawan merupakan mahasiswa di Universitas Pendidikan Nasional dengan program studi teknologi informasi'),
         ),
       ],
-    ),
-    );    
+    );
   }
 }
-
